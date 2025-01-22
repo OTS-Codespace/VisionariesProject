408ebd322cfd45f1bda29653948abbc167c7eac5
Confidence <confycas@gmail.com>
2025-01-22 13:16:59 +0100

    Registered the Models (Category, Customer, Product,Order,OrderItem,Payment,Shipping)

--- myproject/techstore/admin.py
+++ myproject/techstore/admin.py
@@ -1,6 +1,20 @@
 from django.contrib import admin
-from .models import Product, Order, OrderItem
+from .models import Product, Category, Customer, Order, OrderItem, Payment, Shipping
 
 admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Customer)
 admin.site.register(Order)
 admin.site.register(OrderItem)
+admin.site.register(Payment)
+admin.site.register(Shipping)
+
+
+
+
+# from django.contrib import admin
+# from .models import Product, Order, OrderItem
+
+# admin.site.register(Product)
+# admin.site.register(Order)
+# admin.site.register(OrderItem)
