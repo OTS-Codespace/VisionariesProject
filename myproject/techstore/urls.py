7b8ec6e3c63793577c67d3718839bf8cccb2c89f
Confidence <confycas@gmail.com>
2025-01-22 13:43:45 +0100

    added to serve static files

--- myproject/techstore/urls.py
+++ myproject/techstore/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
 from . import views
 from .views import ProductListView,CheckOutView,OrdersView,TrackingView
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
@@ -8,3 +11,6 @@ urlpatterns = [
     path('orders/', OrdersView.as_view(), name='orders'),
     path('tracking/', TrackingView.as_view(), name='tracking'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATICFILES_DIRS[0])
