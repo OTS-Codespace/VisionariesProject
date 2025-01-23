520009e3eaba40072145784abfecf971793bb288
Queency96 <queency96@gmail.com>
2025-01-23 11:16:07 +0100

    updated

--- myproject/techstore/forms.py
+++ myproject/techstore/forms.py
@@ -6,3 +6,12 @@ class CustomerRegistrationForm(UserCreationForm):
     class Meta:
         model = Customer
         fields = ['username', 'first_name', 'last_name', 'email', 'phone_number', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'class': 'form-control'}),
+            'first_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'last_name': forms.TextInput(attrs={'class': 'form-control'}),
+            'email': forms.EmailInput(attrs={'class': 'form-control'}),
+            'phone_number': forms.TextInput(attrs={'class': 'form-control'}),
+            'password1': forms.PasswordInput(attrs={'class': 'form-control', 'type': 'password'}),
+        }
+        
